58a0917c26e5cb22c30047d575c451213feaeb92
Kinrany <kinrany@yandex.com>
2019-10-19 17:15:02 +0300

    Set new state in App#applyGameAction

--- src/App.vue
+++ src/App.vue
@@ -31,8 +31,10 @@ export default {
     "game-ui": Ui
   },
   data: () => ({
-    items: ItemStore.create([Game.Spawn([2, 2]), Game.Robot([3, 2])]),
-    camera: Camera.create()
+    state: {
+      items: ItemStore.create([Game.Spawn([2, 2]), Game.Robot([3, 2])]),
+      camera: Camera.create()
+    }
   }),
   methods: {
     preload(sketch) {
@@ -42,22 +44,25 @@ export default {
       sketch.createCanvas(Camera.CANVAS, Camera.CANVAS);
     },
     draw(sketch) {
-      Camera.draw(sketch, this);
+      Camera.draw(sketch, this.state);
     },
     keyPressed({ keyCode }) {
-      this.applyGameAction(...CONTROL_KEYS.get(keyCode));
+      const action = CONTROL_KEYS.get(keyCode);
+      if (action) {
+        this.applyGameAction(...action);
+      }
     },
     mouseClicked({ mouseX, mouseY }) {
-      const pos = Camera.canvasToTile(this.camera, [mouseX, mouseY]);
+      const pos = Camera.canvasToTile(this.state.camera, [mouseX, mouseY]);
       if (pos) {
         this.applyGameAction("robotAction", "interact", pos);
       }
     },
     getRobot() {
-      return Game.getRobot(this);
+      return Game.getRobot(this.state);
     },
     applyGameAction(...action) {
-      Game.applyGameAction(...action)(this);
+      this.state = Game.applyGameAction(...action)(this.state);
     }
   }
 };
